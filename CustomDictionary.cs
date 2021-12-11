2157a9a8bffb8e4ffacd83249834342eea01e594
firetania <90891114+firetania@users.noreply.github.com>
2021-12-11 14:02:51 +0400

    Add files via upload

--- CustomDictionary.cs
+++ CustomDictionary.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,13 @@ using System.Threading.Tasks;
 
 namespace Task3
 {
-    public class CustomDictionary<TKey, TValue>//: ICollection<KeyValuePair<TKey, TValue>>, IEnumerable<KeyValuePair<TKey, TValue>>, IEnumerable
+    public class CustomDictionary<TKey, TValue>: ICollection<KeyValuePair<TKey, TValue>>, IEnumerable<KeyValuePair<TKey, TValue>>, IEnumerable
     {
         private int size;
+        private int count;
+        private LinkedList<KeyValuePair<TKey, TValue>>[] items;
+        public ICollection<TKey> keys;
+        public ICollection<TValue> values;
         public int Size 
         {
             get 
@@ -16,7 +21,6 @@ namespace Task3
                 return size;
             } 
         }
-        private LinkedList<KeyValuePair<TKey, TValue>>[] items;
         public LinkedList<KeyValuePair<TKey, TValue>>[] Items
         {
             get
@@ -25,62 +29,220 @@ namespace Task3
             }
         }
 
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+        public ICollection<TKey> Keys
+        {
+            get
+            {
+                return keys;
+            }
+        }
+        public ICollection<TValue> Values
+        {
+            get
+            {
+                return values;
+            }
+        }
         public CustomDictionary()
         {
-            size = 255;  // ?
+            size = 4;
+            count = 0;
             items = new LinkedList<KeyValuePair<TKey, TValue>>[size];
+            for (int i = 0; i < items.Length; i++)
+            {
+                items[i] = new LinkedList<KeyValuePair<TKey, TValue>>();
+            }
+            keys = new List<TKey>();
+            values = new List<TValue>();
         }
 
         public CustomDictionary(int size_)
         {
             this.size = size_;
+            count = 0;
             items = new LinkedList<KeyValuePair<TKey, TValue>>[size];
+            for (int i = 0; i < items.Length; i++)
+            {
+                items[i] = new LinkedList<KeyValuePair<TKey, TValue>>();
+            }
+            keys = new List<TKey>();
+            values = new List<TValue>();
         }
 
-        public void Insert(KeyValuePair<TKey, TValue> newItem)
+        public TValue this[TKey key]
         {
-            var hash = newItem.Key.GetHashCode() % size;
-            size++;
-            if (items[hash] == null)
+            get
             {
-                items[hash] = new LinkedList<KeyValuePair<TKey, TValue>>();
-                items[hash].AddLast(newItem);
+                int hash = Math.Abs(key.GetHashCode()) % items.Length;
+
+                if (keys.Contains(key))
+                {
+
+                    foreach (var item in items[hash])
+                    {
+                        if (key.Equals(item.Key))
+                        {
+                            return item.Value;
+                        }
+                    }
+                    return default;
+                }
+                else
+                {
+                    throw new KeyNotFoundException($"No key {key} in the table");
+                }
             }
-            else
+            set
             {
-                items[hash].AddLast(newItem);
+                int hash = Math.Abs(key.GetHashCode()) % items.Length;
+                KeyValuePair<TKey, TValue> newItem = new KeyValuePair<TKey, TValue>(key, value);
+                if (keys.Contains(key))
+                {
+                    
+                    foreach (var item in items[hash])
+                    {
+                        if (key.Equals(item.Key))
+                        {
+                            items[hash].Remove(item);
+                            items[hash].AddLast(newItem);
+                            break;
+                        }
+                    }
+                }
+                else 
+                {
+                    Add(newItem);
+                }
             }
         }
 
-        public void Remove(KeyValuePair<TKey, TValue> existingItem)
+        public void Add(KeyValuePair<TKey, TValue> newItem)
         {
-            var hash = existingItem.Key.GetHashCode() % size;
-            items[hash].Remove(existingItem);
+            var hash = Math.Abs(newItem.Key.GetHashCode()) % items.Length;
+            count++;
+            items[hash].AddLast(newItem);
+            keys.Add(newItem.Key);
+            values.Add(newItem.Value);
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> existingItem)
+        {
+            var hash = Math.Abs(existingItem.Key.GetHashCode()) % items.Length;
+            foreach (var item in items[hash])
+            {
+                if (existingItem.Key.Equals(item.Key))
+                {
+                    items[hash].Remove(existingItem);
+                    count--;
+                    keys.Remove(existingItem.Key);
+                    values.Remove(existingItem.Value);
+                    return true;
+                }
+            }
+            return false;
         }
         public void Clear()
         {
-            size = 0;
             items = new LinkedList<KeyValuePair<TKey, TValue>>[size];
+            for (int i = 0; i < items.Length; i++)
+            {
+                items[i] = new LinkedList<KeyValuePair<TKey, TValue>>();
+            }
+            keys.Clear();
+            values.Clear();
+            count = 0;
         }
         public bool Contains(KeyValuePair<TKey, TValue> itemToFind)
         {
-            var hash = itemToFind.Key.GetHashCode() % size;
+            var hash = Math.Abs(itemToFind.Key.GetHashCode()) % items.Length;
             if (items[hash].Contains(itemToFind))
             {
                 return true;
             }
             return false;
         }
+        public bool ContainsKey(TKey keyToFind)
+        {
+            return keys.Contains(keyToFind);
+        }
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            if (arrayIndex < 0)
+                throw new ArgumentOutOfRangeException("arrayIndex < 0");
+
+            if (array == null)
+                throw new ArgumentNullException("No elements in array");
+
+            if (count > array.Length - arrayIndex - 1)
+                throw new ArgumentException("Not enough space in array");
+
+
+            //for (int i = 0; i < items.Length; i++)
+            //{
+            //    foreach (var item in items[i])
+            //    {
+            //        if (arrayIndex >= array.Length) 
+            //        {
+            //            break;
+            //        }
+            //        array[arrayIndex] = item;
+            //        arrayIndex++;
+            //    }
+            //}
+
+            //array = new KeyValuePair<TKey, TValue>[size - arrayIndex];
+            for (int index = arrayIndex; index < size; ++index)
+            {
+                int curID = 0;
+                foreach (KeyValuePair<TKey, TValue> curItem in items[index])
+                {
+                    array[curID] = curItem;
+                    curID++;
+                }
+            }
+        }
         public void Show()
         {
             foreach(LinkedList<KeyValuePair<TKey, TValue>> curLst in items)
             {
+                if (curLst.Count() > 0)
+                {
+                    foreach (KeyValuePair<TKey, TValue> kvp in curLst)
+                    {
+                        Console.WriteLine("Key = {0}, Value = {1} ", kvp.Key, kvp.Value);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            foreach (LinkedList<KeyValuePair<TKey, TValue>> curLst in items)
+            {
                 foreach (KeyValuePair<TKey, TValue> kvp in curLst)
                 {
-                    Console.WriteLine("Key = {0}, Value = {1} ", kvp.Key, kvp.Value);
+                    yield return kvp;
                 }
-                Console.WriteLine();
             }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,16 +13,37 @@ namespace Task3
             CustomDictionary<string, int> obj = new CustomDictionary<string, int>();
             KeyValuePair<string, int> first = new KeyValuePair<string, int>("Ivanov", 5);
             KeyValuePair<string, int> second = new KeyValuePair<string, int>("Petrov", 4);
-            obj.Insert(first);
-            //obj.Show();
-            foreach (LinkedList<KeyValuePair<string, int>> curLst in obj.Items)
-            {
-                foreach (KeyValuePair<string, int> kvp in curLst)
-                {
-                    Console.WriteLine("Key = {0}, Value = {1} ", kvp.Key, kvp.Value);
-                }
-                Console.WriteLine();
-            }
+            KeyValuePair<string, int> third = new KeyValuePair<string, int>("Sidorov", 3);
+            obj.Add(first);
+            obj.Add(second);
+            obj.Add(third);
+            Console.WriteLine("Add first, second, third");
+            obj.Show();
+
+            obj.Remove(third);
+            Console.WriteLine("Remove third");
+            obj.Show();
+
+            var objCount = obj.Count;
+            Console.WriteLine("Count = {0}", objCount);
+
+            var isItemIvanov5 = obj.Contains(first);
+            Console.WriteLine("Contains Ivanov 5 = {0}", isItemIvanov5);
+
+            var isKeyIvanov = obj.ContainsKey("Ivanov");
+            Console.WriteLine("Contains Ivanov = {0}", isKeyIvanov);
+
+            obj.Clear();
+            Console.WriteLine("Clear");
+            obj.Show();
+
+            KeyValuePair<string, int>[] array = new KeyValuePair<string, int>[3];
+            obj.CopyTo(array, 1);
+            KeyValuePair<string, int> example = new KeyValuePair<string, int>("Sokolov", 2);
+            array[2] = example;
+            Console.WriteLine("Copyto");
+            foreach (var element in array)
+                Console.WriteLine("{0} {1}", element.Key, element.Value);
         }
     }
 }
